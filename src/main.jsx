82a785ffbc255b7d5ec79a5d2bed38556805fc92
Dostonbek Solijonov <youremail@example.com>
2025-11-19 14:11:52 +0500

    home style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes";
+import { routes } from "./routes/index.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes} />
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { useEffect } from "react";
+import Header from "../components/header/Header";
+import SelectCategory from "../components/home-components/SelectCategory";
+import useAxios from "../hooks/useAxios";
+import { useDispatch, useSelector } from "react-redux";
+import { setData } from "../redux/cars-data-slice";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.carsdata);
+  const axios = useAxios();
+
+  const getCars = async () => {
+    let data = await axios({ url: "cars" });
+    dispatch(setData(data.data));
+  };
+  useEffect(() => {
+    getCars();
+  }, []);
+  console.log(data);
+
+  return (
+    <>
+      <Header />
+      <SelectCategory />
+
+      <section className="all-cars">
+        <div className="mycon">
+          {data &&
+            data?.data.map((car) => <div key={car.id}> {car.name} </div>)}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
